ff0709b8525b375cf2844ff64340c9cf488092c2
dvir120041 <98182147+dvir120041@users.noreply.github.com>
2022-01-22 16:59:53 +0200

    separate numbers from large number

---
+++ lesson 1/each number.py
@@ -0,0 +1,10 @@
+num = 4567
+print(num)
+alafim = num//1000
+meot = (num%1000)  //100
+asarot = (num%100) //10
+yehidim = num%10
+print("alafim= " + (str(alafim)))
+print("meot= " + (str(meot)))
+print("asarot= " + (str(asarot)))
+print("yehidim= " + (str(yehidim)))
