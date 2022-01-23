8caed80cd571898ffa557c326cea27dc6889b5c4
dvir1 <dvir120041@gmail.com>
2022-01-23 20:37:13 +0200

    learning drill for string

---
+++ lesson 1/string learning project.py
@@ -0,0 +1,6 @@
+name="dvir sagiv"
+mail="dvir120041@gmail.com"
+age=21
+print("my name is: " + name + "\nmy mail is: " + mail + "\nmy age is: " + (str(age)))
+print((name[::-1] ) + "\n" + (str(age*2)) )
+print("dvir" in "idan ben dudu yuval shimon gal adva shahar yana")
