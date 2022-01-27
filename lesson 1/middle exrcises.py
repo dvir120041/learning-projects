b0326d19d72c00ca4e138b7ff2976026b95c56ec
dvir120041 <98182147+dvir120041@users.noreply.github.com>
2022-01-27 20:51:35 +0200

    simple exrcises

---
+++ lesson 1/middle exrcises.py
@@ -0,0 +1,40 @@
+#first middle exrcise
+# a=input("please enter your full name ")
+# print(a[::-1])
+#second middle exrcise
+# a=input("please enter your file full name ")
+# b=a.split(".")
+# print("your file name is: " + str(b[0]) + "\nyour file type is: " + str(b[1]) )
+#third middle exrcise
+# a=input("please enter a single number")
+# b=a+a
+# c=a+a+a
+# print(int(a) + int(b) +int(c))
+# forth middle exrcise
+# num=input("please enter a number")
+# a=int(num)%2
+# print(a)
+# if(a==0):
+#     print("your number is even")
+# else:
+#     print("your number is odd")
+# fifth middle exrcise
+# n=20
+# d={"x":200}
+# print(type(n)())
+# print(type(d)())
+# sixth exrcise
+from collections import defaultdict
+# str1 = 'w3resource'
+# my_dict = {}
+# for letter in str1:
+#   my_dict[letter] = my_dict.get(letter, 0) + 1
+# print(my_dict)
+#seventh exrcise
+a="abc"
+b="xyz"
+print(a + " " + b)
+c=a(string)
+d=b(string)
+c[1]=d[1]
+d[1]=c[1]
