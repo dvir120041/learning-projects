accd60136e26544b4d1f56fe6098aae3f584262c
dvir120041 <98182147+dvir120041@users.noreply.github.com>
2022-01-30 14:18:03 +0200

    final project

--- lesson 1/final project_def.py
+++ lesson 1/final project_def.py
@@ -1,28 +1,40 @@
 import os
 adds={"www.google.com":"1.1.1.1","www.facebook.com":"2.2.2.2","www.instagram.com":"3.3.3.3"}
 def menu():
-    first_menu=input("welcome to my final project! \nplease choose between the two options: \n1.IP system \n2.dns system")
-    if first_menu=="1":
-        ip_system=input("welcome to the ip system, here are the options for you: \n1.search for ip address in a list"
-                        "\n2.add ip address to a list \n3.delete ip address from the list \n4. print all the ip addresses to the screen")
-        if ip_system=="1":
-            ip_check()
-        if ip_system=="2":
-            ip_add()
-        if ip_system=="3":
-            ip_del()
-        if ip_system=="4":
-            ip_show()
-    elif first_menu=="2":
-        dns_system=input("welccome to the dns system, here are the options for you: \n1.search for a url in a dictionary \n2.add url + ip address to a dictionary"
-                         "\n3.delete url from the dictionary \n4.update ip of a specific url \n5.print all the url:ip to the screen")
-        if dns_system=="1":
-            url_search()
-        elif dns_system=="2":
-            url_ip_add()
-
-
-
+    while(True):
+        first_menu=input("welcome to my final project! \nplease choose between the two options: \n1.IP system \n2.dns system")
+        if first_menu=="1":
+            ip_system=input("welcome to the ip system, here are the options for you: \n1.search for ip address in a list"
+                            "\n2.add ip address to a list \n3.delete ip address from the list \n4. print all the ip addresses to the screen")
+            if ip_system=="1":
+                ip_check()
+            if ip_system=="2":
+                ip_add()
+            if ip_system=="3":
+                ip_del()
+            if ip_system=="4":
+                ip_show()
+        elif first_menu=="2":
+            dns_system=input("welccome to the dns system, here are the options for you: \n1.search for a url in a dictionary \n2.add url + ip address to a dictionary"
+                             "\n3.delete url from the dictionary \n4.update ip of a specific url \n5.print all the url:ip to the screen")
+            if dns_system=="1":
+                url_search()
+            elif dns_system=="2":
+                url_ip_add()
+            elif dns_system=="3":
+                url_del()
+            elif dns_system=="4":
+                ip_change()
+            elif dns_system=="5":
+                url_print()
+            else:
+                print("you entered a wrong number, please try again!")
+        else:
+            print("you entered a wrong number, please try again!")
+        repeat=input("do you want to go again?y/n")
+        if repeat == "n" or repeat == "no":
+            print("ok, goodbye!")
+            break
 def ip_check():
     boolean="true"
     ip_path=("C:/Users/Own/OneDrive/שולחן העבודה/קורס סייבר/קוד לפתיחת קובץ/ip addresses for project.txt")
@@ -53,9 +65,8 @@ def ip_show():
     ip_read = open(ip_path, "r")
     print(ip_read.read())
 def url_search():
-    boolean="true"
     url_ser=input("please enter the url you want to search for")
-    if url_ser in adds == True:
+    if (url_ser in adds) == True:
         print("the url is in the list!")
     else:
         print("the url is not in the list")
@@ -63,8 +74,18 @@ def url_ip_add():
     url=input("please enter the url you want to add")
     ip=input("please enter the ip you want to add")
     adds.update({url:ip})
-
-
+def url_del():
+    url_delete=input("please enter the url you want to delete")
+    if (url_delete in adds) == True:
+        adds.pop(url_delete)
+    else:
+        print("you enterd a url that doesnt exist! please try again")
+def ip_change():
+    url_def=input("please enter the URL")
+    ip_url=input("please enter the new IP")
+    adds[url_def]=ip_url
+def url_print():
+    print(adds)
 
 
 
