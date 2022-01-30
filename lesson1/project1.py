fdab84fc08dec84003d6ccc258064a97dd05bfb0
dvir120041 <98182147+dvir120041@users.noreply.github.com>
2022-01-30 15:48:59 +0200

    first project for medalgim

---
+++ lesson1/project1.py
@@ -0,0 +1,11 @@
+badget=int(input("please enter your marketing budget. \nfacebook campaign = 100ILS per day."
+             "\ninstagram campaign = 50ILS per day."))
+fac_camp=int(input("how long do you want your facebook campaign to go?"))
+inst_camp=int(input("how long do you want your instagram campaign to go?"))
+price=(inst_camp*50 + fac_camp*100)*1.17
+print("your total price + tax will be:" + str(price))
+if(badget>price):
+    print("successfull")
+else:
+    sum=price-badget
+    print("you need to add " + str(sum) + " if you want that campaign ")
